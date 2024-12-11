b664341a4d237fe1159230fd328acfde25e4097f
julie <7jujubuilds@gmail.com>
2024-12-11 14:29:59 +0900

    feat(input): 유효한 월 및 요일 인풋을 받는 플로우 구현

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -2,6 +2,20 @@ package oncall;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        Validator validator = new Validator();
+        String monthInput = this.getMonthInput(inputView, validator);
+    }
+
+    private String getMonthInput(InputView inputView, Validator validator) {
+        while (true) {
+            try {
+                String monthInput = inputView.readMonth();
+                validator.checkMonth(monthInput);
+                return monthInput;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
