7dc1383b35cac542dbf585a9959046f7d5da17f2
julie <7jujubuilds@gmail.com>
2024-12-11 14:21:47 +0900

    feat(validator): 요일 인풋 검증 로직 구현

--- src/main/java/oncall/helpers/Validator.java
+++ src/main/java/oncall/helpers/Validator.java
@@ -2,7 +2,7 @@ package oncall.helpers;
 
 public class Validator {
     private final static String DELIMITER = ",";
-    
+
     public void checkMonth(String input) {
         String monthString = input.split(DELIMITER)[0];
         try {
@@ -11,6 +11,15 @@ public class Validator {
                 throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
             }
         } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
+        }
+    }
+
+
+    public void checkDay(String input) {
+        String dayString = input.split(DELIMITER)[1];
+        List<String> days = List.of("월", "화", "수", "목", "금", "토", "일");
+        if (!days.contains(dayString)) {
             throw new IllegalArgumentException(ErrorMessages.INVALID.getMessage());
         }
     }
